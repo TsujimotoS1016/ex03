3e90e3cf263f8d0f431366d78e9021dec62b4ce7
TsujimotoS1016 <tsujimotos1016@gmail.com>
2024-06-28 21:48:36 +0900

    HoT: initial revision

---
+++ HeadTail.py
@@ -0,0 +1,16 @@
+import random
+
+print('Tossing a coin...')
+Head = 0
+Tail = 0
+for i in range(0,3):
+    num = random.randint(0,1)
+    if(num):
+        value = 'Heads'
+        Head += 1
+    else:
+        value = 'Tails'
+        Tail += 1
+    print('round',i+1,':',value)
+print('Heads:',Head,'Tails:',Tail) 
+    
